f0f483cef56ba74a679de628e43246bf53e11511
Perkles <jade.aabinajm@gmail.com>
2022-01-26 12:07:25 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.getDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.getDescricao("descrição curso js");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria =  new Mentoria();
+        mentoria.setTitulo("mentoria java");
+        mentoria.setDescricao("descrição mentoria java ");
+        mentoria.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+
+
+    }
+}
